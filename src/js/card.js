4e04e24a6551c614562144cdfb2f8c403aed14f5
AVMaslennikov <AVMaslennikov@inno.tech>
2023-10-14 11:39:24 +0300

    fix animated touch event

--- src/js/card.js
+++ src/js/card.js
@@ -1,5 +1,4 @@
 import {isInViewport} from "./utils/detectVisibleElement.js";
-import {getTouchDirection} from "./utils/getTouchDirection.js";
 
 const items = [...document.querySelectorAll('.card__content')];
 const cards = [...document.querySelectorAll('.card')];
@@ -26,55 +25,59 @@ function mouseMove({clientX, clientY}, item) {
 
 	const centerY = top + height / 2;
 	const centerX = left + width / 2;
-	item.nextProgressY = (window.scrollY + clientY - centerY) / w
-	item.nextProgressX = (window.scrollX + clientX - centerX) / h
+	item.nextProgressY = (clientY - centerY) / w
+	item.nextProgressX = (clientX - centerX) / h
+	console.log(window.scrollY, clientY, centerX, top)
 }
 
-dataItems = items.map(el => ({
-	el,
-	animElem: el.querySelector('.card__imgWrapper'),
-	sizes: el.getBoundingClientRect(),
-	isHover: false,
-	progressX: 0,
-	progressY: 0
-}))
+function calculateInitData() {
+	dataItems = items.map(el => ({
+		el,
+		animElem: el.querySelector('.card__imgWrapper'),
+		sizes: el.getBoundingClientRect(),
+		isHover: false,
+		progressX: 0,
+		progressY: 0,
+		nextProgressY: 0,
+		nextProgressX: 0,
+	}))
+}
+
+calculateInitData()
 
 dataItems.forEach((item) => {
 	if (isMobile) {
 		item.el.addEventListener('touchmove', (ev) => touchMove(ev, item), {passive: false})
-		item.el.addEventListener('touchstart', () => item.isHover = true)
+		item.el.addEventListener('touchstart', () => {
+			item.sizes = item.el.getBoundingClientRect()
+			item.isHover = true
+		})
 		item.el.addEventListener('touchend', () => item.isHover = false)
-		return;
+	} else {
+		item.el.addEventListener('mouseenter', () => item.isHover = true)
+		item.el.addEventListener('mouseleave', () => item.isHover = false)
+		item.el.addEventListener('mousemove', (ev) => mouseMove(ev, item))
 	}
-	;
-	item.el.addEventListener('mouseenter', () => item.isHover = true)
-	item.el.addEventListener('mouseleave', () => item.isHover = false)
-	item.el.addEventListener('mousemove', (ev) => mouseMove(ev, item))
 })
 
-
 function animate() {
-	if (!isAnimate) {
-		requestAnimationFrame(animate)
-		return;
-	}
+
 	dataItems.forEach((item) => {
 		if (item.isHover) {
 			item.progressX += (item.nextProgressX - item.progressX) * 0.11;
 			item.progressY += (item.nextProgressY - item.progressY) * 0.11;
 		} else {
-			item.progressX += (0 - item.progressX) * 0.11;
-			item.progressY += (0 - item.progressY) * 0.11;
+			item.progressX += (0 - item.progressX) * 0.11 || 0;
+			item.progressY += (0 - item.progressY) * 0.11 || 0;
 		}
 
-		const forceX = isMobile ? FORCE * 4 : FORCE
-		const forceY = isMobile ? FORCE * 0.6 : FORCE
+		const forceX = isMobile ? FORCE * 6 : FORCE
+		const forceY = isMobile ? FORCE * 0.8 : FORCE
 
 		const rotateX = +(item.progressX * forceX).toFixed(2)
 		const rotateY = +(item.progressY * forceY * -1).toFixed(2)
 
-		const transform = `rotateX(${rotateY}deg) rotateY(${rotateX}deg)`
-		item.animElem.style.transform = transform;
+		item.animElem.style.transform = `rotateX(${rotateY}deg) rotateY(${rotateX}deg)`;
 	})
 	requestAnimationFrame(animate)
 }
