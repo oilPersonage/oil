73959be098076c5748c77dfcfb57b4ba90fa4762
AVMaslennikov <AVMaslennikov@inno.tech>
2023-10-15 07:00:15 +0300

    fix corners

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,12 @@
 import './into.js'
 import './heading.js'
 import './card.js'
+
 import {init} from "./heading.js";
 import {hideOverlay} from "./into.js";
 
 
-document.onreadystatechange = function() {
+document.onreadystatechange = function () {
 	if (document.readyState === 'complete') {
 		hideOverlay()
 		init()
