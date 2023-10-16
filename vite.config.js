0c9dd62808b7b5cffa7382bef12a04537f8c4eb3
AVMaslennikov <AVMaslennikov@inno.tech>
2023-10-17 02:58:06 +0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,10 @@ import {defineConfig} from "vite";
 export default defineConfig({
 	base: "/oil",
 	build: {
-		// sourcemap: true,
+		sourcemap: true,
 		// Reduce bloat from legacy polyfills.
 		target: "esnext",
 		// Leave minification up to applications.
-		// minify: false,
+		minify: false,
 	}
 })
