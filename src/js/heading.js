d17061dd5828b4c08bb1f1f99575d151c7c5c78a
AVMaslennikov <AVMaslennikov@inno.tech>
2024-02-29 18:11:40 +0300

    Исправил баг смены табов

--- src/js/heading.js
+++ src/js/heading.js
@@ -16,25 +16,27 @@ function getRandomInt(min, max) {
 	return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+function animate() {
+	prevIndex = activeIndex;
+	const next = getRandomInt(0, items.length - 1)
+	activeIndex = next === prevIndex ? getRandomInt(0, items.length - 1) : next;
+	anime({
+		targets: animItem,
+		translateY: ['-50%', '-50%'],
+		translateX: [positions[prevIndex], positions[activeIndex]],
+		width: [width[prevIndex], width[activeIndex]],
+		easing: 'cubicBezier(.32,0,.73,1)',
+		duration: 1500 * (Math.abs(prevIndex - activeIndex) * 0.7),
+	})
+}
+
 export function init() {
 	positions = items.reduce((final, curr) => [...final, curr.clientWidth + final[final.length - 1]], [0])
 	width = items.map((el) => el.clientWidth)
 
 	animItem.style.width = items[activeIndex].clientWidth + 'px'
 
-	interval = setInterval(() => {
-		prevIndex = activeIndex;
-		const next = getRandomInt(0, items.length - 1)
-		activeIndex = next === prevIndex ? getRandomInt(0, items.length - 1) : next;
-		anime({
-			targets: animItem,
-			translateY: ['-50%', '-50%'],
-			translateX: [positions[prevIndex], positions[activeIndex]],
-			width: [width[prevIndex], width[activeIndex]],
-			easing: 'cubicBezier(.32,0,.73,1)',
-			duration: 1500 * (Math.abs(prevIndex - activeIndex) * 0.7),
-		})
-	}, DURATION)
+	interval = setInterval(animate, DURATION)
 }
 
 
@@ -44,3 +46,11 @@ window.addEventListener('resize', function resize() {
 
 	animItem.style.width = items[activeIndex].clientWidth + 'px'
 })
+
+document.addEventListener("visibilitychange", function () {
+	if (document.hidden) {
+		clearInterval(interval)
+	} else {
+		interval = setInterval(animate, DURATION)
+	}
+});
